0347f0ee85cd5face72165afd53188fd1927cfa2
xuzhiqiang <youmeiyouai@163.com>
2019-08-06 13:51:44 +0800

    代码初始化

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <van-button type="default">默认按钮</van-button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+  components: {
+  }
+}
+</script>
